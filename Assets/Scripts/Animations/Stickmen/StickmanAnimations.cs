7c3eac89e24c22afb6c124fd0f6c5d188c5a05eb
Nikolai Kirillov <95093978+KlautMagnus@users.noreply.github.com>
2023-03-03 14:17:34 +0300

    Configured Stickman Animation Controller

---
+++ Assets/Scripts/Animations/Stickmen/StickmanAnimations.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Animations.Stickmen
+{
+	[RequireComponent(typeof(Animator))]
+	public class StickmanAnimations : MonoBehaviour
+	{
+		private Animator _animator;
+		private static readonly int IsRuning = Animator.StringToHash("IsRuning");
+
+		private void Start()
+		{
+			_animator = GetComponent<Animator>();
+		
+		}
+		
+		[ContextMenu(nameof(EnterRunState))]
+		public void EnterRunState()
+		{
+			_animator.SetBool(IsRuning, true);
+		}
+	}
+}
